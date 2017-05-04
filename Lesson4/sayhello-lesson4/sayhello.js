9844728942f05ef060d65c9fb9d8faa36639b2b2
naderdl <naderdaliri@gmail.com>
2017-05-04 17:12:09 +0430

    [Delivers #143565743]lesson 4 fnished,npm created.

---
+++ Lesson4/sayhello-lesson4/sayhello.js
@@ -0,0 +1,1 @@
+exports.sayhello = name => `Hello ${name}`
---
+++ Lesson4/server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Hapi = require('hapi');
+const hello = require('sayhello.lesson4')
+const server = new Hapi.Server();
+server.connection({port: 8080, host: 'localhost'});
+server.register([require('vision'), require('inert'), {register: require('lout') }],function(err) {
+});
+server.method({
+ name: 'sayhi',
+method: hello.sayhello,
+});
+server.route({
+ method: 'GET' ,
+ path: '/hello/{name}',
+ handler: (request, reply) => {
+   reply(server.methods.sayhi( encodeURIComponent(request.params.name) ) )
+ },
+ config: {
+   description: 'Says Hello to user by the address bar',
+   notes: 'usage : GET/hellp/{name}',
+   tags: ['Hello', 'API', 'Hapijs', 'server routes']
+ }
+});
+server.start((err) => {
+ if (err) {
+   throw err;
+ }
+ console.log(`Server running at: ${server.info.uri}` );
+});
