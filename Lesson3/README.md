d827d20939011c0998c4ec2d48ad6a4e65df9967
naderdl <naderdaliri@gmail.com>
2017-05-19 02:55:46 +0430

    [Delivers #143565747] fixed name,age bug, and some indentation.

--- Lesson3/README.md
+++ Lesson3/README.md
@@ -1,2 +1,3 @@
-#Lesson3 code
+#Lesson3
+
 This is lesson3. run 'node cb.js --help' in terminal on the project directory and it will guide you it self.
--- Lesson3/cb.js
+++ Lesson3/cb.js
@@ -1,16 +1,21 @@
-const db = new require('puffer')({ host: 'localhost', name: 'default' })
-const Base = require('odme').CB({source:db})
+const db = new require('puffer')({
+  host: 'localhost',
+  name: 'default'
+})
+const Base = require('odme').CB({
+  source: db
+})
 const Joi = require('joi')
 const argv = require('yargs')
 
 class User extends Base {
   props() {
     return {
-      name:{
+      name: {
         schema: Joi.string(),
         whiteList: true
       },
-      age:{
+      age: {
         schema: Joi.number().integer().min(14),
         whiteList: true
       }
@@ -18,24 +23,36 @@ class User extends Base {
   }
 }
 argv
-.usage("$0 save -n=[name] -a=[age]" + '\n' + "$0 get --name=[Document-ID]")
-.command({
-  command: 'save',
-  builder: (argv) => argv.demandOption(['n', 'a']),
-  desc: 'This command saves the Document to bucket.',
-  handler: (argv) => {
-    let user = new User ({name: argv.n, age: argv.a})
-    user.create(true).then( (result, err) => { if(!err){ console.log(result)} } )
-  }
-})
-.command({
-  command: 'get',
-  builder: (argv) => argv.demandOption('name'),
-  desc: 'This command load the Document from bucket.',
-  handler: (argv) => {
-    User.get(argv.name).then( (result, err) => { if(!err){ console.log(result)} } )
-  }
-})
+  .usage("$0 save --name=[name] --age=[age]" + '\n' +
+    "$0 get --name=[Document-ID]")
+  .command({
+    command: 'save',
+    builder: (argv) => argv.demandOption(['name', 'age']),
+    desc: 'This command saves the Document to bucket.',
+    handler: (argv) => {
+      let user = new User({
+        name: argv.name,
+        age: argv.age
+      })
+      user.create(true).then((result, err) => {
+        if (!err) {
+          console.log(result)
+        }
+      })
+    }
+  })
+  .command({
+    command: 'get',
+    builder: (argv) => argv.demandOption('name'),
+    desc: 'This command load the Document from bucket.',
+    handler: (argv) => {
+      User.get(argv.name).then((result, err) => {
+        if (!err) {
+          console.log(result)
+        }
+      })
+    }
+  })
 
 .help()
-.argv
+  .argv
