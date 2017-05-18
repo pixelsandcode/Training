a5c7f837c14ce3fe5aeec1e4203ecf283638a571
naderdl <naderdaliri@gmail.com>
2017-05-19 02:10:48 +0430

    [Delivers #143565751] added part 3, fixed indentation,and dictation problems.

--- Lesson1/lesson1.js
+++ Lesson1/lesson1.js
@@ -1,31 +1,86 @@
- 'use strict';
+'use strict';
 
-const Hapi = require('hapi');
+const Path = require('path')
+const Hapi = require('hapi')
+const Hoek = require('hoek')
 
 const server = new Hapi.Server();
-server.connection({port: 8080, host: 'localhost'});
-server.register([require('vision'), require('inert'), {register: require('lout') }],function(err) {
-});
-const sayhi = function (request, next) {
-  next('Hello ' + encodeURIComponent(request.params.name));
+server.connection({
+  port: 8080,
+  host: 'localhost'
+})
+
+server.register(
+  [
+    require('vision'),
+    require('inert'),
+    require('lout')
+  ], (err) => {
+    Hoek.assert(!err, err)
+
+    server.views({
+      engines: {
+        html: require('handlebars')
+      },
+      relativeTo: __dirname,
+      path: './'
+
+    });
+  }
+)
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    reply("Hello to you my friend.")
+  },
+  config: {
+    description: 'At the start says hello to user.',
+    notes: 'usage : this is the home page.',
+    tags: ['hello', 'startpage', 'route']
+  }
+})
+
+const sayhi = function(request, next) {
+  next(`Hello, ${encodeURIComponent(request.params.name)} .`);
 }
+
 server.method({
   name: 'sayhi',
- method: sayhi,
-});
+  method: sayhi,
+})
+
 server.route({
-  method: 'GET' ,
+  method: 'GET',
   path: '/hello/{name}',
   handler: server.methods.sayhi,
   config: {
-    description: 'Says Hello to user by the address bar',
+    description: 'Says Hello to user by the specified address.',
     notes: 'usage : GET/hellp/{name}',
-    tags: ['Hello', 'API', 'Hapijs', 'server routes']
+    tags: ['route', 'method']
   }
-});
+})
+
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler: (request, reply) => {
+    reply.view('index.html', {
+      hello: 'Hello, ',
+      name: `${encodeURIComponent(request.params.name)}.`
+    })
+  },
+  config: {
+    description: 'Says hello to user by the loading html file.',
+    notes: 'usage : GET/{name}',
+    tags: ['view', 'route', 'html']
+  }
+})
+
 server.start((err) => {
   if (err) {
-    throw err;
+    throw err
   }
-  console.log(`Server running at: ${server.info.uri}` );
-});
+  console.log(`Server running at: ${server.info.uri}`);
+})
--- Lesson1/readme.md
+++ Lesson1/readme.md
@@ -1,5 +1,5 @@
-this is my lesson on answer.
-you should run the API whit nodejs and in terminal you have an localhost now,
-whit http://localhost:8080/ address 
-http://localhost:8080/docs opens the documention of API Routes,
-http://localhost:8080/hello/username says hi to user.
+this is my lesson one answer.
+you should run the lesson1.js with nodejs and in the terminal, now you have a localhost server at http://localhost:8080/ :
+http://localhost:8080/docs opens the documentation of server routes,
+http://localhost:8080/hello/username says hi to the user,
+http://localhost:8080/nader says hi to the user in another way.
