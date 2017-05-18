5ee387be896572e1fce488f54c146d01daaa4d6f
naderdl <naderdaliri@gmail.com>
2017-05-19 02:33:56 +0430

    [Delivers #143565749] fixed name,age bugs.and some indentation.

--- Lesson2/README.md
+++ Lesson2/README.md
@@ -1,1 +1,1 @@
-this is my lesson 2 project just type in terminal node cb.js --help and it will guide you it self.
+This is my lesson 2 answer just type in terminal ```node cb.js --help``` and it will guide you it self.
--- Lesson2/cb.js
+++ Lesson2/cb.js
@@ -1,38 +1,45 @@
 #!/usr/bin/env node
+
 var argv = require('yargs');
 var couchbase = require('couchbase');
 var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
 var bucket = cluster.openBucket('default');
 argv
-.usage("$0 save -n=[name] -a=[age]" + '\n' + "$0 get -n=[docID]")
-.command({
-  command: 'save',
-  desc: 'save a doc in couchbase',
-  builder: (argv) => argv.demandOption(['n','a']),
-  handler: (argv) => {
-    bucket.upsert(argv.n, {name: argv.n, age: argv.a}, function(err, result){if (!err) {
-      console.log('doc saved');
-      bucket.disconnect();
-    }else{
-      console.error('doc didnt saved');
-      bucket.disconnect();}
-    })
-   }
+  .usage("$0 save --name=[name] --age=[age]" + '\n' + "$0 get --name=[docID]")
+  .command({
+    command: 'save',
+    desc: 'Saves the document in couchbase.',
+    builder: (argv) => argv.demandOption(['name', 'age']),
+    handler: (argv) => {
+      bucket.upsert(argv.name, {
+        name: argv.name,
+        age: argv.age
+      }, function(err, result) {
+        if (!err) {
+          console.log('doc saved');
+          bucket.disconnect();
+        } else {
+          console.error('doc didnt saved');
+          bucket.disconnect();
+        }
+      })
+    }
   })
-.command({
-  command: 'get',
-  desc: 'get the doc from couchbase and print it in teminal.',
-  builder: (argv) => argv.demandOption('n'),
-  handler: (argv) => {
-    bucket.get(argv.n, function(err, result) {
-      if(!err) {
-      console.log(result.value);
-      bucket.disconnect();
-    } else {
-      console.error("can't get the doc!");
-      bucket.disconnect();}
-    })
-  }
-})
-.help()
-.argv
+  .command({
+    command: 'get',
+    desc: 'Gets the document from couchbase and print it in teminal.',
+    builder: (argv) => argv.demandOption('name'),
+    handler: (argv) => {
+      bucket.get(argv.name, function(err, result) {
+        if (!err) {
+          console.log(result.value);
+          bucket.disconnect();
+        } else {
+          console.error("can't get the doc!");
+          bucket.disconnect();
+        }
+      })
+    }
+  })
+  .help()
+  .argv
