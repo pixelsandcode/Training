206e74ced097f050976df3b250790771e912c733
naderdl <naderdaliri@gmail.com>
2017-05-19 20:24:50 +0430

    some bug fixed and package name has been changed with kebab case method.

--- Lesson4/server.js
+++ Lesson4/server.js
@@ -1,30 +1,56 @@
 'use strict';
 
 const Hapi = require('hapi');
-const hello = require('sayhello.lesson4')
+const hello = require('say-hello-lesson4')
 const server = new Hapi.Server();
-server.connection({port: 8080, host: 'localhost'});
-server.register([require('vision'), require('inert'), {register: require('lout') }],function(err) {
-});
-server.method({
- name: 'sayhi',
-method: hello.sayhello,
-});
+server.connection({
+  port: 8080,
+  host: 'localhost'
+})
+server.register(
+  [require('vision'),
+    require('inert'),
+    require('lout')
+  ], (err) => {
+    if (err)
+      console.error(err);
+  }
+)
+
 server.route({
- method: 'GET' ,
- path: '/hello/{name}',
- handler: (request, reply) => {
-   reply(server.methods.sayhi( encodeURIComponent(request.params.name) ) )
- },
- config: {
-   description: 'Says Hello to user by the address bar',
-   notes: 'usage : GET/hellp/{name}',
-   tags: ['Hello', 'API', 'Hapijs', 'server routes']
- }
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    reply(`Hello, to you my friend.`)
+  },
+  config: {
+    description: 'Says hello at the start.',
+    notes: 'usage : automated, homepage.',
+    tags: ['hello', 'homepage', 'route']
+  }
+})
+
+server.method({
+  name: 'sayhi',
+  method: hello.sayhello,
+})
+
+server.route({
+  method: 'GET',
+  path: '/hello/{name}',
+  handler: (request, reply) => {
+    reply(server.methods.sayhi(encodeURIComponent(request.params.name)))
+  },
+  config: {
+    description: 'Says Hello to user by the specified address.',
+    notes: 'usage : GET/hellp/{name}',
+    tags: ['Hello', 'API', 'Hapijs', 'server routes']
+  }
 });
+
 server.start((err) => {
- if (err) {
-   throw err;
- }
- console.log(`Server running at: ${server.info.uri}` );
-});
+  if (err) {
+    throw err
+  }
+  console.log(`Server running at: ${server.info.uri}`);
+})
