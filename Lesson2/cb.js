20e5c6fd22dc8dcfc2bc8b1479d1b2b2e94e8cef
naderdl <naderdaliri@gmail.com>
2017-05-26 00:08:52 +0430

    fixed indentation.

--- Lesson2/cb.js
+++ Lesson2/cb.js
@@ -5,56 +5,49 @@ var couchbase = require('couchbase');
 var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
 var bucket = cluster.openBucket('default');
 argv
-  .usage("$0 save --name=[name] --age=[age]" + '\n' + "$0 get --name=[docID]")
-  .alias('n', 'name')
-  .alias('a', 'age')
-  .command(
-  {
-    command: 'save',
-    desc: 'Saves the document in couchbase.',
-    builder: (argv) => argv.demandOption(['name', 'age']),
-    handler: (argv) =>
-    {
-      bucket.upsert(argv.name,
-      {
-        name: argv.name,
-        age: argv.age
-      }, function(err, result)
-      {
-        if (!err)
-        {
-          console.log('doc saved');
-          bucket.disconnect();
-        }
-        else
-        {
-          console.error('doc didnt saved');
-          bucket.disconnect();
-        }
-      })
-    }
-  })
-  .command(
-  {
-    command: 'get',
-    desc: 'Gets the document from couchbase and print it in teminal.',
-    builder: (argv) => argv.demandOption('name'),
-    handler: (argv) =>
-    {
-      bucket.get(argv.name, function(err, result)
-      {
-        if (!err)
-        {
-          console.log(result.value);
-          bucket.disconnect();
-        }
-        else
-        {
-          console.error("can't get the doc!");
-          bucket.disconnect();
-        }
-      })
-    }
-  })
-  .help()
-  .argv
+
+.usage("$0 save --name=[name] --age=[age]" + '\n' + "$0 get --name=[docID]")
+
+.alias('n', 'name')
+.alias('a', 'age')
+
+.command({
+  command: 'save',
+  desc: 'Saves the document in couchbase.',
+  builder: (argv) => argv.demandOption(['name', 'age']),
+  handler: (argv) => {
+    bucket.upsert(argv.name, {
+      name: argv.name,
+      age: argv.age
+    }, function(err, result) {
+      if (!err) {
+        console.log('doc saved');
+        bucket.disconnect();
+      } else {
+        console.error('doc didnt saved');
+        bucket.disconnect();
+      }
+    })
+  }
+})
+
+.command({
+  command: 'get',
+  desc: 'Gets the document from couchbase and print it in teminal.',
+  builder: (argv) => argv.demandOption('name'),
+  handler: (argv) => {
+    bucket.get(argv.name, function(err, result) {
+      if (!err) {
+        console.log(result.value);
+        bucket.disconnect();
+      } else {
+        console.error("can't get the doc!");
+        bucket.disconnect();
+      }
+    })
+  }
+})
+
+.help()
+
+.argv
--- Lesson3/cb.js
+++ Lesson3/cb.js
@@ -1,27 +1,21 @@
-const db = new require('puffer')(
-{
+const db = new require('puffer')({
   host: 'localhost',
   name: 'default'
 })
-const Base = require('odme').CB(
-{
+const Base = require('odme').CB({
   source: db
 })
 const Joi = require('joi')
 const argv = require('yargs')
 
-class User extends Base
-{
-  props()
-  {
+class User extends Base {
+  props() {
     return {
-      name:
-      {
+      name: {
         schema: Joi.string(),
         whiteList: true
       },
-      age:
-      {
+      age: {
         schema: Joi.number().integer().min(14),
         whiteList: true
       }
@@ -29,47 +23,42 @@ class User extends Base
   }
 }
 argv
-  .usage("$0 save --name=[name] --age=[age]" + '\n' +
-    "$0 get --name=[Document-ID]")
-  .alias('n', 'name')
-  .alias('a', 'age')
-  .command(
-  {
-    command: 'save',
-    builder: (argv) => argv.demandOption(['name', 'age']),
-    desc: 'This command saves the Document to bucket.',
-    handler: (argv) =>
-    {
-      let user = new User(
-      {
-        name: argv.name,
-        age: argv.age
-      })
-      user.create(true).then((result, err) =>
-      {
-        if (!err)
-        {
-          console.log(result)
-        }
-      })
-    }
-  })
-  .command(
-  {
-    command: 'get',
-    builder: (argv) => argv.demandOption('name'),
-    desc: 'This command load the Document from bucket.',
-    handler: (argv) =>
-    {
-      User.get(argv.name).then((result, err) =>
-      {
-        if (!err)
-        {
-          console.log(result)
-        }
-      })
-    }
-  })
+
+.usage("$0 save --name=[name] --age=[age]" + '\n' +
+  "$0 get --name=[Document-ID]")
+
+.alias('n', 'name')
+.alias('a', 'age')
+
+.command({
+  command: 'save',
+  builder: (argv) => argv.demandOption(['name', 'age']),
+  desc: 'This command saves the Document to bucket.',
+  handler: (argv) => {
+    let user = new User({
+      name: argv.name,
+      age: argv.age
+    })
+    user.create(true).then((result, err) => {
+      if (!err) {
+        console.log(result)
+      }
+    })
+  }
+})
+
+.command({
+  command: 'get',
+  builder: (argv) => argv.demandOption('name'),
+  desc: 'This command load the Document from bucket.',
+  handler: (argv) => {
+    User.get(argv.name).then((result, err) => {
+      if (!err) {
+        console.log(result)
+      }
+    })
+  }
+})
 
 .help()
-  .argv
+.argv
