d615396d8b1cfe3e08e2c20a72fac5d6dafe3893
naderdl <naderdaliri@gmail.com>
2017-05-25 16:23:04 +0430

    [Delivers #143565749] alias bug fixed. dictation mistakes fixed.

--- Lesson2/README.md
+++ Lesson2/README.md
@@ -1,1 +1,1 @@
-This is my lesson 2 answer just type in terminal ```node cb.js --help``` and it will guide you it self.
+This is my lesson2 answer just type in terminal ```node cb.js --help``` and it will guide you it self.
--- Lesson2/cb.js
+++ Lesson2/cb.js
@@ -6,35 +6,50 @@ var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
 var bucket = cluster.openBucket('default');
 argv
   .usage("$0 save --name=[name] --age=[age]" + '\n' + "$0 get --name=[docID]")
-  .command({
+  .alias('n', 'name')
+  .alias('a', 'age')
+  .command(
+  {
     command: 'save',
     desc: 'Saves the document in couchbase.',
     builder: (argv) => argv.demandOption(['name', 'age']),
-    handler: (argv) => {
-      bucket.upsert(argv.name, {
+    handler: (argv) =>
+    {
+      bucket.upsert(argv.name,
+      {
         name: argv.name,
         age: argv.age
-      }, function(err, result) {
-        if (!err) {
+      }, function(err, result)
+      {
+        if (!err)
+        {
           console.log('doc saved');
           bucket.disconnect();
-        } else {
+        }
+        else
+        {
           console.error('doc didnt saved');
           bucket.disconnect();
         }
       })
     }
   })
-  .command({
+  .command(
+  {
     command: 'get',
     desc: 'Gets the document from couchbase and print it in teminal.',
     builder: (argv) => argv.demandOption('name'),
-    handler: (argv) => {
-      bucket.get(argv.name, function(err, result) {
-        if (!err) {
+    handler: (argv) =>
+    {
+      bucket.get(argv.name, function(err, result)
+      {
+        if (!err)
+        {
           console.log(result.value);
           bucket.disconnect();
-        } else {
+        }
+        else
+        {
           console.error("can't get the doc!");
           bucket.disconnect();
         }
