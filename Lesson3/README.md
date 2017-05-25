13d69c4421e0e902b117c86ea9121e96f60bc551
naderdl <naderdaliri@gmail.com>
2017-05-25 16:37:55 +0430

    [Delivers #143565747] alias bug fixed.

--- Lesson3/README.md
+++ Lesson3/README.md
@@ -1,3 +1,3 @@
 #Lesson3
 
-This is lesson3. run 'node cb.js --help' in terminal on the project directory and it will guide you it self.
+This is lesson3 answer. run 'node cb.js --help' in terminal on the project directory and it will guide you it self.
--- Lesson3/cb.js
+++ Lesson3/cb.js
@@ -1,21 +1,27 @@
-const db = new require('puffer')({
+const db = new require('puffer')(
+{
   host: 'localhost',
   name: 'default'
 })
-const Base = require('odme').CB({
+const Base = require('odme').CB(
+{
   source: db
 })
 const Joi = require('joi')
 const argv = require('yargs')
 
-class User extends Base {
-  props() {
+class User extends Base
+{
+  props()
+  {
     return {
-      name: {
+      name:
+      {
         schema: Joi.string(),
         whiteList: true
       },
-      age: {
+      age:
+      {
         schema: Joi.number().integer().min(14),
         whiteList: true
       }
@@ -25,29 +31,40 @@ class User extends Base {
 argv
   .usage("$0 save --name=[name] --age=[age]" + '\n' +
     "$0 get --name=[Document-ID]")
-  .command({
+  .alias('n', 'name')
+  .alias('a', 'age')
+  .command(
+  {
     command: 'save',
     builder: (argv) => argv.demandOption(['name', 'age']),
     desc: 'This command saves the Document to bucket.',
-    handler: (argv) => {
-      let user = new User({
+    handler: (argv) =>
+    {
+      let user = new User(
+      {
         name: argv.name,
         age: argv.age
       })
-      user.create(true).then((result, err) => {
-        if (!err) {
+      user.create(true).then((result, err) =>
+      {
+        if (!err)
+        {
           console.log(result)
         }
       })
     }
   })
-  .command({
+  .command(
+  {
     command: 'get',
     builder: (argv) => argv.demandOption('name'),
     desc: 'This command load the Document from bucket.',
-    handler: (argv) => {
-      User.get(argv.name).then((result, err) => {
-        if (!err) {
+    handler: (argv) =>
+    {
+      User.get(argv.name).then((result, err) =>
+      {
+        if (!err)
+        {
           console.log(result)
         }
       })
