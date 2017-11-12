4bc48cdf09834c5f326007b090a9e9347c1ed2c3
Amin Abbasi <amin4193@gmail.com>
2017-11-12 10:22:02 +0330

    lesson11[delivers#152372120]

---
+++ amin/lessons/lesson11/elastic.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const ES = require('elasticsearch')
+const client = new ES.Client({host: 'localhost:9200'})
+//client.indices.putMapping({})
+client.indices.create({
+  index: 'users_jwt',
+  body: {
+    mappings: {
+      user: {
+        properties: {
+          doc: {
+            properties: {
+              email: {
+                type: 'string',
+                index: 'not_analyzed'
+              },
+              password: {
+                type: 'string',
+                index: 'not_analyzed'
+              },
+              fullname: {
+                type: 'string'
+              },
+              dob: {
+                type: 'string'
+              },
+              weight: {
+                type: 'string'
+              },
+              height: {
+                type: 'string'
+              },
+              docKey: {
+                type: 'string'
+              },
+              docType: {
+                type: 'string'
+              }
+            }
+          }
+        }
+      },
+      jwt: {
+        properties: {
+          doc: {
+            properties: {
+              token: {
+                type: 'string',
+                index: 'not_analyzed'
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}, (err, res) => {
+  if (err) {
+    console.error(`\n---------ERROR IN CREATE INDICES:\n${JSON.stringify(err, null, 2)}`)
+  } else {
+    console.log(`\n---------SUCCESS IN CREATE INDICES:\n${JSON.stringify(res, null, 2)}`)
+  }
+})
