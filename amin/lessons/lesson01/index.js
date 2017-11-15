434f355c9ef5081cceb5184a4e18bd74a1993289
Amin Abbasi <amin4193@gmail.com>
2017-11-15 11:47:03 +0330

    modify lesson01

--- amin/lessons/lesson01/index.js
+++ amin/lessons/lesson01/index.js
@@ -7,21 +7,53 @@ server.connection({
   port: 3000
 })
 
-function sayhi(name) {
-  return 'Hello ' + name + '! :-D'
+// Handler function of route 'GET'
+const say_hi = function (request, reply) {
+  reply(`Hello ${request.params.name}! :-D`)
 }
 
 server.register([require('vision'), require('inert'), {register: require('lout')}], function (err) {
-});
-
-// Add the route
-server.route({
-  method: 'GET',
-  path: '/hello/{name}',
-  handler: function (request, reply) {
-    reply(sayhi(request.params.name))
-  }
+  if (err)
+    console.log(err)
 })
+
+// Add routes
+server.route([
+  {
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      reply('----- Home Page -----\n\nHello World! :-D')
+    },
+    config: { // Config for lout to have a good documentation!
+      description: 'This is a home page.',
+      notes: 'This page gets {name} and says: "Hello {name}"',
+      tags: ['get', 'home']
+    }
+  },
+  {
+    method: 'GET',
+    path: '/hello',
+    handler: function (request, reply) {
+      reply('Hello Stranger! :-D')
+    },
+    config: {
+      description: 'This is a hello page.',
+      notes: 'This page says: "Hello Stranger!"',
+      tags: ['get', 'home', 'hello']
+    }
+  },
+  {
+    method: 'GET',
+    path: '/hello/{name}',
+    handler: say_hi,
+    config: {
+      description: 'This is a hello page.',
+      notes: 'This page gets {name} and says: "Hello {name}"',
+      tags: ['get', 'home', 'hello']
+    }
+  }
+])
 
 // Start the server
 server.start(function (err) {
