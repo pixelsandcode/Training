aaf22692540fe03c21fd5419bc199a895a9fa049
Amin Abbasi <amin4193@gmail.com>
2017-11-15 17:52:35 +0330

    modify lesson02

--- amin/lessons/lesson02/index.js
+++ amin/lessons/lesson02/index.js
@@ -2,31 +2,40 @@ const couchbase = require('couchbase')
 const cluster = new couchbase.Cluster('couchbase://127.0.0.1')
 const bucket = cluster.openBucket('default')
 
-console.log(bucket)
-
 const argv = require('yargs')
+  .usage('$0 save [name] [age]' +
+         '$0 get [name] ')
   .alias('n', 'name')
   .alias('a', 'age')
-  .command(
-    'save',
-    'save a doc in couchbase', 
-    { },
-    function (argv) {
-      bucket.upsert('u:3', {name: argv.name, age: argv.age}, function (err, result) {
-        if (err) throw err
-        console.log('upsert success')
+  .command({
+    command: 'save',
+    aliases: 's',
+    builder: (argv) => argv.demandOption(['n', 'a']),
+    desc: 'This command saves a Document into bucket.',
+    handler: (argv) => {
+      bucket.upsert(argv.name, {name: argv.name, age: argv.age}, (err, res) => {
+        if (err)
+          console.log(JSON.stringify(err, null, 2))
+        else
+          console.log('Create doc successful:\n', JSON.stringify(res, null, 2))
       })
-    })
-  .command(
-    'get',
-    'get a doc from couchbase with specific name',
-    { },
-    function (argv) {
-      bucket.get(argv.name, function (err, result) {
-        if (err) throw err
-        console.log(result.value)
-      });
-    })
+    }
+  })
+  .command({
+    command: 'get',
+    aliases: 'g',
+    builder: (argv) => argv.demandOption('n'),
+    desc: 'This command gets a doc from couchbase with specific name.',
+    handler: (argv) => {
+      bucket.get(argv.name, (err, res) => {
+        if (err)
+          console.log(JSON.stringify(err, null, 2))
+        else
+          console.log(JSON.stringify(res, null, 2))
+      })
+    }
+  })
+  .help('h')
   .argv
 
 
