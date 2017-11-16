8dab6fdb564022786377f31d6e10b6b7c9305c0b
Amin Abbasi <amin4193@gmail.com>
2017-11-16 10:55:37 +0330

    modify lesson07

--- amin/lessons/lesson07/index.js
+++ amin/lessons/lesson07/index.js
@@ -1,21 +1,16 @@
 const Puffer = require('puffer')
-const db = new Puffer({
-  host: 'localhost',
-  name: 'default'
-})
+const db = new Puffer({host: 'localhost', name: 'default'})
 
 const ODME = require('odme')
 const Model = ODME.CB({source: db})
 
 const ES = require('elasticsearch')
-const client = new ES.Client({
-  host: 'localhost:9200',
-  //log: 'trace'
-})
+const client = new ES.Client({host: 'localhost:9200', log: 'error'})
 
 const Joi = require('joi')
 
 class User extends Model {
+
   _mask() {
     return 'name,dob'
   }
@@ -38,6 +33,101 @@ class User extends Model {
   }
 }
 
+const delete_index = () => {
+  if (client.indices.exists({index: 'users'})) {
+    client.indices.delete({
+      index: 'users'
+    }, (err, res) => {
+      if (err) {
+        console.log(`\nERROR IN DELETE:\n${JSON.stringify(err, null, 2)}`)
+      } else {
+        console.log(`\nDELETE USER INDEX:\n${JSON.stringify(res, null, 2)}`)
+      }
+    })
+  }
+}
+
+const create_index = () => {
+  client.indices.create({
+    index: 'users',
+    body: {
+      mappings: {
+        user: {
+          properties: {
+            doc: {
+              properties: {
+                name: {
+                  type: 'string',
+                  index: 'not_analyzed'
+                },
+                dob: {
+                  type: 'string'
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }, (err, res) => {
+    if (err) {
+      console.log(`\nERROR IN CREATE INDICES:\n${JSON.stringify(err, null, 2)}`)
+    } else {
+      console.log(`\nSUCCESS IN CREATE INDICES:\n${JSON.stringify(res, null, 2)}`)
+    }
+  })
+}
+
+const create_user = (argv) => {
+  const user = new User({
+    name: argv.name,
+    dob: argv.dob
+  })
+  user.create(true).then((res, err) => {
+    if (err) {
+      console.log(`\nERROR IN DOCS:\n${JSON.stringify(err, null, 2)}`)
+    } else {
+      console.log(`\nADD NEW USER:\n${JSON.stringify(res, null, 2)}`)
+    }
+  })
+}
+
+const search_all = () => {
+  client.search({
+    index: 'users',
+    type: 'user',
+    query: {
+      match_all: {}
+    }
+  }, (err, res) => {
+    if (err) {
+      console.log(`\nERROR IN SEARCH:\n${JSON.stringify(err, null, 2)}`)
+    } else {
+      console.log(`\nALL USERS:\n${JSON.stringify(res.hits.hits, null, 2)}`)
+    }
+  })
+}
+
+const search_user = (argv) => {
+  client.search({
+    index: 'users',
+    type: 'user',
+    body: {
+      filter: {
+        term: {
+          "doc.name": argv.name
+        }
+      }
+    }
+  }, (err, res) => {
+    if (err) {
+      console.log(`\nERROR IN SEARCH:\n${JSON.stringify(err, null, 2)}`)
+    } else {
+      console.log(`\nUSERS WITH NAME='${argv.name}':\n${JSON.stringify(res, null, 2)}`)
+    }
+  })
+}
+
 const argv = require('yargs')
   .usage(`$0 delete-index
           $0 create-index
@@ -53,19 +143,7 @@ const argv = require('yargs')
     aliases: 'di',
     builder: {},
     desc: 'Deletes "users" index form elasticsearch.',
-    handler: () => {
-      if (client.indices.exists({index: 'users'})) {
-        client.indices.delete({
-          index: 'users'
-        }, (err, res) => {
-          if (err) {
-            console.log(`\n---------ERROR IN DELETE:\n${JSON.stringify(err, null, 2)}`)
-          } else {
-            console.log(`\n---------DELETE USER INDEX:\n${JSON.stringify(res, null, 2)}`)
-          }
-        })
-      }
-    }
+    handler: delete_index
   })
 
   .command({
@@ -73,36 +151,7 @@ const argv = require('yargs')
     aliases: 'ci',
     builder: {},
     desc: 'Creates "users" index.',
-    handler: () => {
-      client.indices.create({
-        index: 'users',
-        body: {
-          mappings: {
-            users: {
-              properties: {
-                doc: {
-                  properties: {
-                    name: {
-                      type: 'string',
-                      index: 'not_analyzed'
-                    },
-                    dob: {
-                      type: 'string'
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }, (err, res) => {
-        if (err) {
-          console.log(`\n---------ERROR IN CREATE INDICES:\n${JSON.stringify(err, null, 2)}`)
-        } else {
-          console.log(`\n---------SUCCESS IN CREATE INDICES:\n${JSON.stringify(res, null, 2)}`)
-        }
-      })
-    }
+    handler: create_index
   })
 
   .command({
@@ -110,74 +159,23 @@ const argv = require('yargs')
     aliases: 'cu',
     builder: (argv) => argv.demandOption('n', 'd'),
     desc: 'Creates doc in "users" index.',
-    handler: (argv) => {
-      const user = new User({
-        name: argv.name,
-        dob: argv.dob
-      })
-      user.create(true).then((res, err) => {
-        if (err) {
-          console.log(`\n---------ERROR IN DOCS:\n${JSON.stringify(err, null, 2)}`)
-        } else {
-          console.log(`\n---------ADD NEW USER:\n${JSON.stringify(res, null, 2)}`)
-        }
-      })
-    }
+    handler: create_user
   })
 
   .command({
-    command: 'search-all-user',
-    aliases: 'a',
+    command: 'search-all',
+    aliases: 'sa',
     builder: {},
     desc: 'Returns all users from index.',
-    handler: () => {
-      client.search({
-        index: 'users',
-        //q: 'type:user'
-        docType: 'user',
-        query: {
-          match_all: {}
-          //type: 'user'
-        }
-      // client.search({
-      //   body: {}
-      }, (err, res) => {
-        if (err) {
-          console.log(`\n---------ERROR IN SEARCH:\n${JSON.stringify(err, null, 2)}`)
-        } else {
-          console.log(`\n---------ALL USERS:\n${JSON.stringify(res, null, 2)}`)
-        }
-      })
-      console.log('search all users')
-    }
+    handler: search_all
   })
 
   .command({
-    command: 'search-user-name',
-    aliases: 's',
+    command: 'search-user',
+    aliases: 'su',
     builder: (argv) => argv.demandOption('n'),
     desc: 'Returns users with exact name from "users".',
-    handler: (argv) => {
-      client.search({
-        index: 'users',
-        docType: 'user',
-        //type: 'user',
-        body: {
-          filter: {
-            term: {
-              "doc.name": argv.name
-            }
-          }
-        }
-      }, (err, res) => {
-        if (err) {
-          console.log(`\n---------ERROR IN SEARCH:\n${JSON.stringify(err, null, 2)}`)
-        } else {
-          console.log(`\n---------LIST OF USERS WITH NAME='${argv.name}':\n
-          ${JSON.stringify(res, null, 2)}`)
-        }
-      })
-    }
+    handler: search_user
   })
 
   .help('h')
